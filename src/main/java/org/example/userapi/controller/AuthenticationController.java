1698f0b321a3ca282c669e1cd59169b7c4778d6d
sameh-tarek <tarksame7@gmail.com>
2024-04-14 01:00:10 +0200

    add cros origin

--- src/main/java/org/example/userapi/controller/AuthenticationController.java
+++ src/main/java/org/example/userapi/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.example.userapi.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/users/auth")
 @RequiredArgsConstructor
--- src/main/java/org/example/userapi/controller/ValidationController.java
+++ src/main/java/org/example/userapi/controller/ValidationController.java
@@ -1,10 +1,12 @@
 package org.example.userapi.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("users/token/validation")
 public class ValidationController {
